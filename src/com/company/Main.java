eb9055d22c4226acd59ee37d6513008f48c05a76
unknown <ksh04132@mju.ac.kr>
2021-12-13 17:11:29 +0900

    Selection Sort

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] list = new int[n];
+
+        for(int h = 0 ; h < list.length; h++) list[h] = sc.nextInt();
+
+        int temp;
+        for(int i = 0; i < list.length; i++){
+            int min = list[i];
+            int minN = i;
+            for(int j = i+1; j < list.length; j++){
+                if(list[j] < min) {
+                    min = list[j];
+                    minN = j;
+                }
+            }
+            temp = list[i];
+            list[i] = list[minN];
+            list[minN] = temp;
+        }
+        for(int k = 0; k < list.length; k++) System.out.print(list[k] + " ");
+    }
+}
