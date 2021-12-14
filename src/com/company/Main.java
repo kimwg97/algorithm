ee71a9220b055fa8bed237a133c366636ea741f7
kimwg97 <kimwg97@gmail.com>
2021-12-14 22:10:28 +0900

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // Main main = new Main();
-        //main.sort();                  메인에서 정렬을 사용할 때 쓰이는 코드입니다.
+        // main.sort();                  메인에서 정렬을 사용할 때 쓰이는 코드입니다.
     }
 
     public void sort() {
