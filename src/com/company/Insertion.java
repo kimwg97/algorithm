4c6748e2db02a4dbae7297bde589ff9e8b3d3707
kimwg97 <kimwg97@gmail.com>
2021-12-16 23:27:38 +0900

    add insertion sort

---
+++ src/com/company/Insertion.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Insertion {
+    public void sort(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] list = new int[n];
+
+        for(int  h = 0; h < n; h++) list[h] = sc.nextInt();
+        int temp;
+        for(int i = 0; i < n - 1; i++){
+            for(int j = i; j >= 0; j--){
+                if(list[j+1] < list[j]){
+                    temp = list[j+1];
+                    list[j+1] = list[j];
+                    list[j] = temp;
+                }
+            }
+        }
+        for(int k = 0; k < n; k++) System.out.print(list[k] + " ");
+    }
+}
