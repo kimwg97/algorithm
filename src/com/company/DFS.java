142aff0700cf36813f65e2ef6f8c14962dafb6fa
kimwg97 <kimwg97@gmail.com>
2022-01-05 23:59:57 +0900

    dd

--- src/com/company/DFS.java
+++ src/com/company/DFS.java
@@ -1,7 +1,10 @@
 package com.company;
 
 public class DFS {
-    public void dfs(int l, int[] arr){      // 모든 부분 집합 구하기, l = 0, arr 은 연속된 자연수
+    int count = 0;
+    int max = 0;
+
+    public void dfs1(int l, int[] arr){      // 모든 부분 집합 구하기, l = 0, arr 은 연속된 자연수
         if(l == arr.length){
             for(int i = 0; i < arr.length; i++)
                 if(arr[i] == 1) System.out.print(i+1 + " ");
@@ -9,9 +12,22 @@ public class DFS {
         }
         else{
             arr[l] = 1;
-            dfs(l+1, arr);
+            dfs1(l+1, arr);
             arr[l] = 0;
-            dfs(l+1, arr);
+            dfs1(l+1, arr);
         }
+    }
+
+    public int dfs2(int[] arr, int n, int c, int sum){
+        if(count > c || sum == n-1){
+            if(count > c) count -= arr[sum-1];
+            if(max < count) max = count;
+        }else{
+            count += arr[sum];
+            dfs2(arr, n, c, sum+1);
+            count -= arr[sum];
+            dfs2(arr, n, c, sum+1);
+        }
+        return max;
     }
 }
