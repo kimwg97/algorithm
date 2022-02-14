995274c5a114c1b0f1c4f8cccd54e7fad0c4981e
kimwg97 <kimwg97@gmail.com>
2022-02-14 15:56:28 +0900

    try N1759

--- src/Baekjoon/N1759.java
+++ src/Baekjoon/N1759.java
@@ -1,11 +1,9 @@
 package Baekjoon;
 
-import com.company.DFS;
-
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class N1759 {
-    int consonant = 0, gather = 0;
     public void N1759(){
         Scanner sc = new Scanner(System.in);
         int l = sc.nextInt();
@@ -14,18 +12,19 @@ public class N1759 {
         char[] result = new char[l];
 
         for(int i = 0; i < c; i++) chars[i] = sc.next().charAt(0);
-
-        DFS(chars, result, 0, 0);
+        Arrays.sort(chars);
+        DFS(chars, result, 0, 0, 0, 0);
     }
 
-    public void DFS(char[] chars, char[] result, int l, int c){
-        if(l == result.length){
+    public void DFS(char[] chars, char[]result, int l, int c, int gather, int consonant){
+        if(l == result.length && gather >= 1 && consonant >= 2){
             for(char x : result) System.out.print(x + " ");
             System.out.println();
         }else{
             for(int i = c; i < chars.length; i++){
                 result[l] = chars[i];
-                DFS(chars, result, l+1, i+1);
+                if(chars[i] == 'a' || chars[i] == 'e' || chars[i] == 'i' || chars[i] == 'o' || chars[i] == 'u') DFS(chars, result, l+1, i+1, gather+1, consonant);
+                else DFS(chars, result, l+1, i+1, gather, consonant+1);
             }
         }
     }
