e87c3469f9270ce4756330e3a091bf1cd73d0fa3
kimwg97 <kimwg97@gmail.com>
2022-02-21 20:13:12 +0900

    solution BaekJoon N15684, but cheating

--- src/Baekjoon/N15684.java
+++ src/Baekjoon/N15684.java
@@ -3,8 +3,9 @@ package Baekjoon;
 import java.util.Scanner;
 
 public class N15684 {
-    int n, m, h;
+    int n, m, h, count;
     int[][] arr;
+    boolean flag = false;
 
     public void N15684(){
         Scanner sc = new Scanner(System.in);
@@ -13,13 +14,55 @@ public class N15684 {
         h = sc.nextInt();
         arr = new int[h+1][n+1];
 
-        for(int i = 0; i < m; i++){
-            arr[i][0] = sc.nextInt();
-            arr[i][1] = sc.nextInt();
+        for(int i = 0; i < m; i++) {
+            int y = sc.nextInt();
+            int x = sc.nextInt();
+
+            arr[y][x] = 1;
+            arr[y][x+1] = 2;
         }
+
+        for(int i = 0; i <= 3; i++){
+            count = i;
+            DFS(0);
+            if(flag) {
+                System.out.println(count);
+                break;
+            }
+        }
+        if(!flag) System.out.println(-1);
+
     }
 
-    public void DFS(){
-        
+    public boolean check(){
+        for(int j = 1; j <= n; j++) {
+            int start = j;
+            for (int i = 1; i <= h; i++) {
+                if (arr[i][start] == 1) start++;
+                else if (arr[i][start] == 2) start--;
+            }
+            if(j != start) return false;
+        }
+        return true;
+    }
+
+    public void DFS(int l){
+        if(flag) return;
+        if(count == l){
+            if(check()) flag = true;
+            return;
+        }else{
+            for(int i = 1; i <= h; i++){
+                for(int j = 1; j < n; j++){
+                    if(arr[i][j] == 0 && arr[i][j+1] == 0){
+                        arr[i][j] = 1;
+                        arr[i][j+1] = 2;
+                        DFS(l+1);
+                        arr[i][j] = 0;
+                        arr[i][j+1] = 0;
+                    }
+                }
+            }
+        }
     }
 }
