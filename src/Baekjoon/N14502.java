f7f80fd49be3a510bb3da84adfc155c86a38a5a6
kimwg97 <kimwg97@gmail.com>
2022-02-27 23:30:43 +0900

    try N14502

--- src/Baekjoon/N14502.java
+++ src/Baekjoon/N14502.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class N14502 {
     int n, m;
     int[][] map;
-    boolean[][] check;
+    int[][] check;
+
+    int[] dx = {1, 0, -1, 0};
+    int[] dy = {0, -1, 0, 1};
 
     public void N14502(){
         Scanner sc = new Scanner(System.in);
@@ -20,8 +23,8 @@ public class N14502 {
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(map[i][j] == 0){
-                    check = new boolean[n][m];
-                    DFS(i, j);
+                    check = map.clone();
+                    DFS(i, j, 0);
                 }
             }
         }
@@ -37,7 +40,27 @@ public class N14502 {
     // 세개 다 세웠으면 2로 채우고(다른 함수)
     // 0개수 세고
     // 백트래킹 하면서 다른 곳에 벽을 세워가면서 반복
-    public void DFS(int x, int y){
+    public void DFS(int x, int y, int l){
+        if(l == 3){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < m; j++) System.out.print(check[i][j]);
+                System.out.println();
+            }
+            System.out.println();
+            return;
+        }else{
+            for(int i = x; i < n; i++){
+                for(int j = y; j < m; j++){
+                    for(int h = 0; h < 4; h++) {
+                        if(check[i][j] == 0){
+                            check[i][j] = 3;
+                            DFS(i, j, l+1);
+                            check[i][j] = 0;
+                        }
+                    }
+                }
+            }
 
+        }
     }
 }
