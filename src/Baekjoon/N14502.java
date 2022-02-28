ae478791338395b5dbe81c9db26af6492c4797f4
kimwg97 <kimwg97@gmail.com>
2022-02-28 23:51:06 +0900

    try N14502 2

--- src/Baekjoon/N14502.java
+++ src/Baekjoon/N14502.java
@@ -6,6 +6,9 @@ public class N14502 {
     int n, m;
     int[][] map;
     int[][] check;
+    int[][] vir;
+    int count = 0;
+    int max = Integer.MIN_VALUE;
 
     int[] dx = {1, 0, -1, 0};
     int[] dy = {0, -1, 0, 1};
@@ -29,34 +32,58 @@ public class N14502 {
             }
         }
 
+        System.out.println(max);
     }
 
-    public void virus(){
-
+    public void virus(int x, int y){
+        for(int i = 0; i < 4; i++){
+            int nx = dx[i] + x;
+            int ny = dy[i] + y;
+            if(nx >= 0 && ny >= 0 && ny < m && nx < n && vir[nx][ny] == 0){
+                vir[nx][ny] = 3;
+                virus(nx, ny);
+            }
+        }
     }
 
-    // 위에서 for 문으로 DFS 호출
-    // 벽은 무조건 세개 세워야 하므로 먼저 세개 세우고
-    // 세개 다 세웠으면 2로 채우고(다른 함수)
-    // 0개수 세고
-    // 백트래킹 하면서 다른 곳에 벽을 세워가면서 반복
     public void DFS(int x, int y, int l){
         if(l == 3){
+            // 벽이 세개 세워지면 해당 배열을 클론하여 2를 3으로 감염시킴
+            vir = check.clone();
             for(int i = 0; i < n; i++){
-                for(int j = 0; j < m; j++) System.out.print(check[i][j]);
+                for(int j = 0; j < m; j++){
+                    if(vir[i][j] == 2) {
+                        vir[i][j] = 3;
+                        virus(i, j);
+                    }
+                }
+            }
+
+            // 안전 지역 0을 세는 부분
+            System.out.println("vir: ");
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < m; j++){
+                    System.out.print(vir[i][j] + " ");
+                    if(vir[i][j] == 0) count++;
+                }
                 System.out.println();
             }
             System.out.println();
-            return;
+            System.out.println("check: ");
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < m; j++) System.out.print(check[i][j] + " ");
+                System.out.println();
+            }
+            System.out.println();
+            max = Math.max(max, count);
+            count = 0;
         }else{
-            for(int i = x; i < n; i++){
-                for(int j = y; j < m; j++){
-                    for(int h = 0; h < 4; h++) {
-                        if(check[i][j] == 0){
-                            check[i][j] = 3;
-                            DFS(i, j, l+1);
-                            check[i][j] = 0;
-                        }
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < m; j++){
+                    if(check[i][j] == 0){
+                        check[i][j] = 4;
+                        DFS(i, j, l+1);
+                        check[i][j] = 0;
                     }
                 }
             }
