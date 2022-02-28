e3f02bf5a83bea44182be8ea9734db9099ee1491
kimwg97 <kimwg97@gmail.com>
2022-03-01 01:52:54 +0900

    solution BaekJoon N14502

--- src/Baekjoon/N14502.java
+++ src/Baekjoon/N14502.java
@@ -1,11 +1,13 @@
 package Baekjoon;
 
+import java.awt.*;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class N14502 {
     int n, m;
     int[][] map;
-    int[][] check;
     int[][] vir;
     int count = 0;
     int max = Integer.MIN_VALUE;
@@ -18,76 +20,71 @@ public class N14502 {
         n = sc.nextInt();
         m = sc.nextInt();
         map = new int[n][m];
+        vir = new int[n][m];
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++) map[i][j] = sc.nextInt();
         }
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(map[i][j] == 0){
-                    check = map.clone();
-                    DFS(i, j, 0);
-                }
-            }
-        }
+        DFS(0);
 
         System.out.println(max);
     }
 
-    public void virus(int x, int y){
-        for(int i = 0; i < 4; i++){
-            int nx = dx[i] + x;
-            int ny = dy[i] + y;
-            if(nx >= 0 && ny >= 0 && ny < m && nx < n && vir[nx][ny] == 0){
-                vir[nx][ny] = 3;
-                virus(nx, ny);
+    public void safe(){
+        // 안전 지역 0을 세는 부분
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(vir[i][j] == 0) count++;
             }
         }
+        max = Math.max(max, count);
+        count = 0;
     }
 
-    public void DFS(int x, int y, int l){
-        if(l == 3){
-            // 벽이 세개 세워지면 해당 배열을 클론하여 2를 3으로 감염시킴
-            vir = check.clone();
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < m; j++){
-                    if(vir[i][j] == 2) {
-                        vir[i][j] = 3;
-                        virus(i, j);
-                    }
-                }
-            }
+    public void virus(){
+        // 벽이 세개 세워지면 해당 배열을 클론하여 2를 3으로 감염시킴
+        Queue<Point> q = new LinkedList<>();
 
-            // 안전 지역 0을 세는 부분
-            System.out.println("vir: ");
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < m; j++){
-                    System.out.print(vir[i][j] + " ");
-                    if(vir[i][j] == 0) count++;
+        for(int h = 0; h < map.length; h++){ // 반복문 + ArrayCopy
+            System.arraycopy(map[h], 0, vir[h], 0, vir[h].length);
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++) if(vir[i][j] == 2){
+                vir[i][j] = 3;
+                q.add(new Point(i, j));
+            }
+        }
+
+        while (!q.isEmpty()){
+            Point temp = q.poll();
+
+            for(int i = 0; i < 4; i++){
+                int nx = dx[i] + temp.x;
+                int ny = dy[i] + temp.y;
+                if(nx >= 0 && ny >= 0 && ny < m && nx < n && vir[nx][ny] == 0){
+                    vir[nx][ny] = 3;
+                    q.add(new Point(nx, ny));
                 }
-                System.out.println();
             }
-            System.out.println();
-            System.out.println("check: ");
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < m; j++) System.out.print(check[i][j] + " ");
-                System.out.println();
-            }
-            System.out.println();
-            max = Math.max(max, count);
-            count = 0;
+        }
+
+        safe();
+    }
+
+    public void DFS(int l){
+        if(l == 3){
+            virus();
         }else{
             for(int i = 0; i < n; i++){
                 for(int j = 0; j < m; j++){
-                    if(check[i][j] == 0){
-                        check[i][j] = 4;
-                        DFS(i, j, l+1);
-                        check[i][j] = 0;
+                    if(map[i][j] == 0){
+                        map[i][j] = 4;
+                        DFS(l+1);
+                        map[i][j] = 0;
                     }
                 }
             }
-
         }
     }
 }
