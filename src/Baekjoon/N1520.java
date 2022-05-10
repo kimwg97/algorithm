3eafd81c1b8b9fcbc7680c695559844f10ab61fe
kimwg97 <kimwg97@gmail.com>
2022-05-11 01:21:45 +0900

    solution BaekJoon N1520

--- src/Baekjoon/N1520.java
+++ src/Baekjoon/N1520.java
@@ -7,7 +7,6 @@ public class N1520 {
     int n, m;
     int[][] map;
     int[][] dp;
-    int count = 0;
 
     int[] dx = {0, 1, 0, -1};
     int[] dy = {-1, 0, 1, 0};
@@ -25,32 +24,33 @@ public class N1520 {
             }
         }
 
-        int start = map[0][0];
-        map[0][0] = 10001;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                dp[i][j] = -1;
+            }
+        }
 
-        DFS(0, 0, start);
-
-        System.out.println(count);
-
+        System.out.println(DFS(0, 0));
     }
 
-    public void DFS(int x, int y, int high){
-        if(x == n-1 && y == m-1){
-            count++;
-            return;
-        }else {
+    public int DFS(int x, int y) {
+        if (x == n - 1 && y == m - 1) return 1;
+
+        if (dp[x][y] != -1) return dp[x][y];
+        else {
+            dp[x][y] = 0;
             for (int i = 0; i < 4; i++) {
                 int nx = dx[i] + x;
                 int ny = dy[i] + y;
 
-                if(nx >= 0 && ny >=0 && nx < n && ny < m && high > map[nx][ny]){
-                    int next = map[nx][ny];
-                    map[nx][ny] = 10001;
-                    DFS(nx, ny, next);
-                    map[nx][ny] = next;
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m && map[x][y] > map[nx][ny]) {
+                    dp[x][y] += DFS(nx, ny);
                 }
             }
         }
+
+        return dp[x][y];
     }
+
 
 }
