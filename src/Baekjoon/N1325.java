e480c5e25ae32265dfe53e72f790a3d710ac70ea
kimwg97 <kimwg97@gmail.com>
2022-05-25 17:17:00 +0900

    solution BaekJoon N1325

--- src/Baekjoon/N1325.java
+++ src/Baekjoon/N1325.java
@@ -1,65 +1,74 @@
 package Baekjoon;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class N1325 {
 
     int n, m;
-    int[] dp;
-    int[][] computer;
-    ArrayList<Integer> best = new ArrayList<>();
-
-    int[] dx = {0, 1, 0, -1};
-    int[] dy = {1, 0, -1, 0};
+    int[] visit;
+    int[] ans;
+    ArrayList<Integer>[] computer;
 
     public void N1325(){
         Scanner sc = new Scanner(System.in);
 
         n = sc.nextInt();
         m = sc.nextInt();
-        dp = new int[n+1];
-        dp[1] = 1;
-        computer = new int[n+1][n+1];
+        ans = new int[n+1];
+        computer = new ArrayList[n+1];
+
+        for(int i = 0; i <= n; i++) computer[i] = new ArrayList<>();
 
         for(int i = 0; i < m; i++){
             int a = sc.nextInt();
             int b = sc.nextInt();
 
-            computer[b][a] = 1;
+            computer[a].add(b);
         }
+
         int max = 0;
-        for(int i = 1; i <= n; i++){
-            DFS(i);
-            max = Math.max(max, dp[i]);
-        }
 
         for(int i = 1; i <= n; i++){
-            if(dp[i] == max) best.add(i);
+            visit = new int[n+1];
+            BFS(i);
         }
 
-        Collections.sort(best);
+        for(int i = 1; i <= n; i++) max = Math.max(max, ans[i]);
 
-        for(int i : best){
-            System.out.println(i);
-        }
-
+        for(int i = 1; i <= n; i++) if(ans[i] == max) System.out.print(i + " ");
     }
 
-    public int DFS(int c){
+    public void BFS(int i)
+    {
+        Queue<Integer> q = new LinkedList<Integer>();
 
-        if(dp[c] != 0 ) return dp[c];
-        else {
-            for (int i = 1; i <= n; i++) {
-                if (computer[c][i] == 1) {
-                    dp[c] += DFS(i) + 1;
+        q.add(i);
+        visit[i] = 1;
+        while (!q.isEmpty())
+        {
+            int temp = q.poll();
+            for (int x : computer[temp])
+            {
+                if (visit[x] == 0)
+                {
+                    ans[x]++;
+                    visit[x] = 1;
+                    q.add(x);
                 }
             }
         }
+    }
 
-        return dp[c];
+    public void DFS(int c, int start){
+        for(int x : computer[c]){
+            if(visit[x] == 0){
+                visit[x] = 1;
+                ans[x]++;
+                DFS(x, start);
+            }
+        }
     }
 }
